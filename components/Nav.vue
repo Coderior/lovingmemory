751dff686cc67cdfc204c63a2c2cd7187331b602
Kode <kodestar@gmail.com>
2021-01-08 22:08:58 +0000

    Remove coming soon

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
         <ul>
         <li><NuxtLink to="/"><span class="icon"><i class="fas fa-h-square fa-fw" /></span>Home</NuxtLink></li>
-        <li><NuxtLink to="/order-of-service"><span class="icon"><i class="fas fa-map fa-fw" /></span><span class="menu">Order of Service<small>Coming soon</small></span></NuxtLink></li>
+        <li><NuxtLink to="/order-of-service"><span class="icon"><i class="fas fa-map fa-fw" /></span><span class="menu">Order of Service</span></NuxtLink></li>
         <li><NuxtLink to="/live-stream"><span class="icon"><i class="fab fa-youtube fa-fw" /></span><span class="menu">Service Live Stream</span></NuxtLink></li>
         <li><NuxtLink to="/gallery"><span class="icon"><i class="fas fa-images fa-fw" /></span><span class="menu">Gallery</span></NuxtLink></li>
         <li><NuxtLink to="/charity-donation"><span class="icon"><i class="fas fa-hands-helping fa-fw" /></span><span class="menu">Charity Donations</span></NuxtLink></li>
