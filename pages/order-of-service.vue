ac8f2bfda5483d9ed471d90784d960fa8ca8fc13
Kode <kodestar@gmail.com>
2021-01-11 12:07:35 +0000

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -9,13 +9,32 @@
       <p class="highlight">Those who are able to attend please be aware that it was Lyn's wish that people not wear black at her funeral, so feel free to dress as colourfully as you like.</p>
       <h2>Church Service <span>10:30am</span></h2>
       <ul class="fa-ul">
-        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Service starts at 10:30am at Connection Church Centre - Kingshurst</li>
-        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>More details coming soon!</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Service starts at 10:30am at Connection Church Centre - Kingshurst - Due to covid this is by invite only</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Entrance music: And Can It Be</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Scripture reading - Chris Beard</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Welcome &amp; Opening Prayer - Chris Beard</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Lords Prayer - Chris Beard</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Tributes - Family &amp; Friends</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Jesus Is My Anchor - Song written by Lyn a few months ago and set to music by kevin. Lyn wanted it played today.</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Few moments of quiet</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Short message - Chris Beard</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Prayer of Commendation</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Closing prayer &amp; Grace blessing</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Exit music: New Wine</li>
       </ul>
       <h2>Crematorium <span>12:00pm</span></h2>
       <ul class="fa-ul">
-        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Service starts at 12pm at Woodlands Crematorium</li>
-        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>More details coming soon!</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Service starts at 12pm at Woodlands Crematorium - Due to covid this is by invite only</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Entrance music: And Can It Be</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Scripture reading - Psalm 61 v1-4</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Welcome &amp; Opening Prayer - Chris Beard</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Tributes - Family &amp; Friends</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Jesus Is My Anchor - Song written by Lyn a few months ago and set to music by kevin. Lyn wanted it played today.</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Few moments of quiet</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Short message - Chris Beard</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Committal</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Closing prayer &amp; Grace blessing</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Exit music: New Wine</li>
       </ul>
     </div>
   </div>
