5e74eb818bdbaa5ef1a79b3f5ab85fc823d5ad47
Kode <kodestar@gmail.com>
2021-01-11 12:18:45 +0000

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="section details">
       <h1 class="title">Order of service</h1>
-      <p><em>Thursday 28th January 2021</em> Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a get together at a later date when COVID-19 restrictions are relaxed.</p>
+      <p><em>Thursday 28th January 2021</em> Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of Lyns life later in the year when COVID-19 restrictions are relaxed.</p>
       <p class="highlight">Those who are able to attend please be aware that it was Lyn's wish that people not wear black at her funeral, so feel free to dress as colourfully as you like.</p>
       <h2>Church Service <span>10:30am</span></h2>
       <ul class="fa-ul">
@@ -14,8 +14,9 @@
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Scripture reading - Chris Beard</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Welcome &amp; Opening Prayer - Chris Beard</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Lords Prayer - Chris Beard</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Song: Goodenss of God</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Tributes - Family &amp; Friends</li>
-        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Jesus Is My Anchor - Song written by Lyn a few months ago and set to music by kevin. Lyn wanted it played today.</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Jesus Is My Anchor - Song written by Lyn a few months ago and set to music by Kevin. Lyn wanted it played today.</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Few moments of quiet</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Short message - Chris Beard</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Prayer of Commendation</li>
@@ -28,8 +29,8 @@
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Entrance music: And Can It Be</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Scripture reading - Psalm 61 v1-4</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Welcome &amp; Opening Prayer - Chris Beard</li>
-        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Tributes - Family &amp; Friends</li>
-        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Jesus Is My Anchor - Song written by Lyn a few months ago and set to music by kevin. Lyn wanted it played today.</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Tribute</li>
+        <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Jesus Is My Anchor - Song written by Lyn a few months ago and set to music by Kevin. Lyn wanted it played today.</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Few moments of quiet</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Short message - Chris Beard</li>
         <li><span class="fa-li"><i class="fas fa-angle-right"></i></span>Committal</li>
