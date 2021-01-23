ae41de15c3f345a02243c38901f42999a021e9d2
Kode <kodestar@gmail.com>
2021-01-23 14:58:21 +0000

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -3,8 +3,10 @@
 cd assets/images/
 for n in *
 do
-OldName=$n
-NewName=`echo $n | tr -cd 'A-Za-z0-9._-'`
-echo $NewName
-mv "$OldName" "$NewName"
+  OldName=$n
+  NewName=`echo $n | tr -cd 'A-Za-z0-9._-'`
+  if [ "$OldName" != "$NewName" ]; then
+    echo $NewName
+    mv "$OldName" "$NewName"
+  fi
 done
