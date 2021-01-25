4abf5907d360da07d3305ade7c5ffadb8bcdc324
Kode <kodestar@gmail.com>
2021-01-25 14:40:32 +0000

    remove width on front image

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="section tribute">
       <h1 class="title">In Loving Memory</h1>
-      <img width="244" height="292" src="~/assets/frontimage.jpg" alt="In loving memory" />
+      <img height="292" src="~/assets/frontimage.jpg" alt="In loving memory" />
       <div class="subtitle"><div class="name">{{ $config.name }}</div>{{ $config.dates }}</div>
     </div>
   </div>
