b3e4750463c9710849ccf8c32f9c886522862ea1
KodeStar <kodestar@gmail.com>
2022-06-10 16:24:15 +0100

    Update order-of-service.vue

--- pages/order-of-service.vue
+++ pages/order-of-service.vue
@@ -4,6 +4,7 @@
       <Nav />
     </div>
     <div class="section details">
+      <!-- CONTENT START -->
       <h1 class="title">Order of service</h1>
       <p><em>Thursday 28th January 2021</em> Due to COVID-19 the services are restricted in the number of people allowed. The plan is to live stream the services so everyone who wants to attend will be able to do so virtually. We will also hold a celebration of Lyns life later in the year when COVID-19 restrictions are relaxed.</p>
       <p class="highlight">A Bit of important text that you want to stand out, such as any dressing requirements.</p>
@@ -34,6 +35,7 @@
         <ListItem>Closing prayer &amp; Grace blessing</ListItem>
         <ListItem>Exit music: New Wine</ListItem>
       </ul>
+      <!-- CONTENT END -->
     </div>
   </div>
 </template>
