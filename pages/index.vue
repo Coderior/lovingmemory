22785b63cfbc8efa00f6cb485994142a36609b68
KodeStar <kodestar@gmail.com>
2022-06-10 16:23:21 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,11 @@
       <Nav />
     </div>
     <div class="section tribute">
+      <!-- CONTENT START -->
       <h1 class="title">In Loving Memory</h1>
       <img height="292" src="~/assets/frontimage.jpg" alt="In loving memory" />
       <div class="subtitle"><div class="name">{{ $config.name }}</div>{{ $config.dates }}</div>
+      <!-- CONTENT END -->
     </div>
   </div>
 </template>
